3f828d69b0329f741ad21142b16a909015caca7b
SeanZhan <zhanlixiang0729@163.com>
2015-11-24 15:34:24 +0800

    适配不同设备的启动界面

---
+++ NewsReader/Controller/Page/HomePage.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewsReader
+//
+//  Created by zhan on 15/11/24.
+//  Copyright © 2015年 湛礼翔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomePage : UIViewController
+
+
+@end
+
