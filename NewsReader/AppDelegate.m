1557a51ab284c4ee708061d166f4762b6c409523
SeanZhan <zhanlixiang0729@163.com>
2015-11-24 11:26:12 +0800

    搭建系统框架

---
+++ NewsReader/AppDelegate.m
@@ -0,0 +1,39 @@
+//
+//  AppDelegate.m
+//  NewsReader
+//
+//  Created by zhan on 15/11/24.
+//  Copyright © 2015年 湛礼翔. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+  return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application
+{
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+}
+
+@end
---
+++ NewsReader/Defines.h
@@ -0,0 +1,13 @@
+//
+//  Defines.h
+//  NewsReader
+//
+//  Created by zhan on 15/11/24.
+//  Copyright © 2015年 湛礼翔. All rights reserved.
+//
+
+#ifndef Defines_h
+#define Defines_h
+
+
+#endif /* Defines_h */
